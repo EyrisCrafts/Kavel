1f50a83c86bcf059e81b5e0a65745b459c3a6398
K-Rafiki <waleed.nizamaniwn@gmail.com>
2018-03-22 17:41:57 -0700

    Added Firebase Database and tables

--- kavel/src/app/app.module.ts
+++ kavel/src/app/app.module.ts
@@ -9,6 +9,18 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+const config = {
+  apiKey: "AIzaSyB0hy5tjtghl0HlNvOHb5oVwT_hvvpKrhw",
+  authDomain: "my-awesome-project-9511c.firebaseapp.com",
+  databaseURL: "https://my-awesome-project-9511c.firebaseio.com",
+  projectId: "my-awesome-project-9511c",
+  storageBucket: "my-awesome-project-9511c.appspot.com",
+  messagingSenderId: "968110543209"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +30,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- kavel/src/pages/home/home.ts
+++ kavel/src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Observable} from 'rxjs/Rx';
+
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  gDay: string = "Monday";
+  day: string = "Monday";
+  time: string = "Breakfast";
+  dish:string ="";
+  items:Observable<any[]>;
+  BreakfastFood:Observable<any[]>;
+  LunchFood:Observable<any[]>;
+  DinnerFood:Observable<any[]>;
+  constructor(public navCtrl: NavController, public fdb : AngularFireDatabase) {
+//      this.items = this.fdb.list("cuisines").valueChanges();
+//      console.log(this.items);
+//    this.items = this.fdb.list("cuisines").snapshotChanges().map(changes=>{
+//      return changes.map(c=>({ key: c.payload.key, ...c.payload.val()
+//      }));
+//    });
   }
-
+  btnClicked(){
+    this.fdb.list("cuisines/"+this.day+"/"+this.time).push({Time:this.time, Dish:this.dish, Day:this.day});
+    this.dish="";
+  }
+  receiveData(){
+    this.items = this.fdb.list("cuisines").snapshotChanges().map(changes=>{
+      return changes.map(c=>({ key: c.payload.key, ...c.payload.val()
+      }));
+    });
+  }
+  delete(item){
+    this.fdb.list("cuisines/"+item.Day+"/"+item.Time).remove(item.key);
+  }
+  giff(gDay){
+    this.BreakfastFood = this.fdb.list("cuisines/"+gDay+"/Breakfast").snapshotChanges().map(changes=>{
+      return changes.map(c=>({ key: c.payload.key, ...c.payload.val()
+      }));
+    });
+    this.LunchFood = this.fdb.list("cuisines/"+gDay+"/Lunch").snapshotChanges().map(changes=>{
+      return changes.map(c=>({ key: c.payload.key, ...c.payload.val()
+      }));
+    });
+    this.DinnerFood = this.fdb.list("cuisines/"+gDay+"/Dinner").snapshotChanges().map(changes=>{
+      return changes.map(c=>({ key: c.payload.key, ...c.payload.val()
+      }));
+    });
+  }
 }
